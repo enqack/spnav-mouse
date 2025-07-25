7437f1f3022527bcfefd4c9f5a4746d7021628e7
enqack <enqack@gmail.com>
2025-07-25 11:54:41 -0400

    Initial flake.nix.

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "SpaceMouse to mouse control bridge";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          default = spnav-mouse;
+
+          spnav-mouse = pkgs.stdenv.mkDerivation {
+            pname = "spnav-mouse";
+            version = "0.5";
+
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              libspnav
+            ];
+
+            makeFlags = [
+              "PREFIX=$(out)"
+              "DESTDIR="
+            ];
+
+            meta = with pkgs.lib; {
+              description = "SpaceMouse to mouse control bridge utility";
+              homepage = "https://github.com/enqack/spnav-mouse";
+              license = "BSD-3-Clause";
+              platforms = platforms.linux;
+              maintainers = [ ];
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gcc
+            gnumake
+          ];
+
+          buildInputs = with pkgs; [
+            libspnav
+          ];
+
+          # Add any environment variables needed for development
+          shellHook = ''
+            echo "spnav-mouse development shell"
+          '';
+        };
+      }
+    );
+}
